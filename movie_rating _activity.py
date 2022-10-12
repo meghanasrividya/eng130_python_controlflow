0fc27bbfc7638f2af185e68770a139a66605710e
meghanasrividya <meghanar19@gmail.com>
2022-10-12 15:52:39 +0100

    added the changes

--- movie_rating _activity.py
+++ movie_rating _activity.py
@@ -8,13 +8,17 @@
 # if nothing matched inform the user to enter their age again
 # max user must not be allowed to enter over 117 years
 
-age=input("Please , enter your age:")
+age=int(input("Please , enter your age:"))
 
-if age>=18 :
+if age >= 18 and age <= 117:
     print("You are allowed to watch all the movies")
-elif age>=16 and age < 18 :
+elif age >= 16 :
     print("These movies are only for 16 years and above ")
-elif age>=15 and age < 16 :
+elif age >= 15 :
     print("These movies are only for 15 years and above")
-elif age>=12 and age <15 :
-    print("")
+elif age >= 12 :
+    print("These movies are only for 12 and above")
+elif age > 117:
+    print("Please enter the correct age")
+else:
+    print("please enter only digits")
